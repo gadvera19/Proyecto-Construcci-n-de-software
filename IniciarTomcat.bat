147b4304a35680ba8afabc36edb8772b30c548d5
gadvera19 <127161616+gadvera19@users.noreply.github.com>
2026-06-17 18:40:18 -0500

    Add files via upload

---
+++ IniciarTomcat.bat
@@ -0,0 +1,5 @@
+@echo off
+set JAVA_HOME=C:\Program Files\Apache NetBeans\jdk
+set CATALINA_HOME=C:\apache-tomcat-10.1.55\apache-tomcat-10.1.55
+C:\apache-tomcat-10.1.55\apache-tomcat-10.1.55\bin\startup.bat
+pause
